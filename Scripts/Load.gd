5bab2c5757f95d51717885502f55fe124c631c10
Oyale Salami <salamivictor15@gmail.com>
2024-07-30 17:15:02 +0100

    Optimizes the project by downsizing the textures

--- Scripts/Load.gd
+++ Scripts/Load.gd
@@ -1,9 +1,9 @@
 extends Control
 
 #5 seconds till the game loads
-var timer = 5 
+var timer = 4 
 
 func _process(delta):
 	timer -= delta
 	if(timer <= 0):
-		get_tree().change_scene("res://Scenes/Main.tscn")
+		var _scene = get_tree().change_scene("res://Scenes/Main.tscn")
--- Scripts/Planet.gd
+++ Scripts/Planet.gd
@@ -3,7 +3,7 @@ extends Spatial
 var r
 func _ready():
 	randomize()
-	r = randi() % 6
+	r = randi() % 7
 	if(r == 0):
 		get_node("Planet 1").visible = true
 	elif(r == 1):
@@ -14,5 +14,7 @@ func _ready():
 		get_node("Planet 4").visible = true
 	elif(r == 4):
 		get_node("Planet 5").visible = true
-	else:
+	elif(r == 5):
 		get_node("Planet 6").visible = true
+	else:
+		get_node("Planet 7").visible = true
